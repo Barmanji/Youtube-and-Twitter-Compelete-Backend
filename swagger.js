d2ba7380db544748ea89b737f28d9696a6cd6f0a
Ajay Barman <barmanjiaj@gmail.com>
2026-01-22 22:44:28 +0530

    feat(Swagger.js): Added Doc AutoGen Swagger at /doc

---
+++ swagger.js
@@ -0,0 +1,21 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    version: '1.2.0',            // by default: '1.0.0'
+    title: 'Youtube_Twitter_Backend',              // by default: 'REST API'
+    description: 'API For Backend of Youtube and Twitter.'         // by default: ''
+  },
+    host: 'localhost:8000',                 // by default: 'localhost:3000'
+  basePath: '',             // by default: '/'
+  // schemes: [],              // by default: ['http']
+  // consumes: [],             // by default: ['application/json']
+  // produces: [],             // by default: ['application/json']
+  // securityDefinitions: {},  // by default: empty object
+  // definitions: {}           // by default: empty object
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./src/app.js'];
+
+swaggerAutogen()(outputFile, routes, doc);
