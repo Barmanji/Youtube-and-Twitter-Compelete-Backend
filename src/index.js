c03d6cdeb08fcd270b1f4f218a7321d5306cbb0c
Ajay Barman <barmanjiaj@gmail.com>
2026-01-23 10:45:14 +0530

    style(Comments and Commas): Removed

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
-//require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { DB_NAME } from "./constants.js";
 import { app } from "./app.js";
 dotenv.config({
     path: './.env'
 })
-console.log(process.env)
-//;(async ()=>{
-//    try {
-//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//        app.on("error",(error)=>{
-//            console.log('ERR: ', error);
-//            throw error
-//        })
-//        app.listen(process.env.PORT, ()=>{
-//            console.log(`The app is listening at ${process.env.PORT}`);
-//        })
-//    }
-//    catch {
-//        console.log("error",error)
-//        throw err
-//    }
-//})()
 
 connectDB()
     .then(() => {
